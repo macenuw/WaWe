e852e44fc3cb7d0839a065f390e09693d112eb23
macenuw <gnykabiny54@gmail.com>
2021-01-13 21:49:11 +0200

    added sticky header

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 $(function() {
+    $(".menu a, .logo").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
     
     $('.slider').slick({
         dots: true,
         arrows: false,
     });
 
-    $('.menu__btn').on('click',function(){
+    $('.menu__btn, .menu a').on('click',function(){
         $('.menu__list').toggleClass('menu__list--active');
     });
 
